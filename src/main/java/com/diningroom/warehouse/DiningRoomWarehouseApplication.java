25d57f0cefc61d5e6bb4a4e9787d2d218d615b68
GustavoBonif <gubonic@gmail.com>
2023-11-22 20:28:39 -0300

    setup: creating stock service

---
+++ src/main/java/com/diningroom/warehouse/DiningRoomWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.diningroom.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningRoomWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningRoomWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diningroom/warehouse/DiningRoomWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diningroom.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningRoomWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
